92e6edb1f045de7e8315de402ed753cf5d3027d4
Vadim <vadim-zue@lenta.ru>
2024-05-18 19:15:45 +0300

    init

---
+++ TheGame/ClassOpenWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TheGame
+{
+    public static class ClassOpenWindow
+    {
+        public static void OpenWindow(this Window pref, Window Opened)
+        {
+            Opened.Show();
+            pref.Hide();
+            Opened.Closing += (x, y) => pref.Show();
+        }
+    }
+}
