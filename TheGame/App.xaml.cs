af106a4d831ac9dc932b19a8ae386260d8649967
pstlnce <galvanistic.reanimation@yandex.com>
2024-05-19 19:11:38 +0300

    db update

--- TheGame/App.xaml.cs
+++ TheGame/App.xaml.cs
@@ -1,17 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows;
+using TheGame.Database;
+using TheGame.DependencyInjection;
 
-namespace TheGame
+namespace TheGame;
+
+/// <summary> Логика взаимодействия для App.xaml </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
-    public partial class App : Application
+    private ServiceCollection _services;
+
+    public App()
     {
+        _services = new ServiceCollection();
+        ConfigureServices(_services);
+    }
+
+    private static void ConfigureServices(ServiceCollection services)
+    {
+        const string DB_PATH = "GameStore.db";
+
+        services.AddDbContext<GameDbContext>(options =>
+        {
+            options.UseSqlite($"Data Source={DB_PATH}");
+        });
+
+        var dbSets = typeof(GameDbContext)
+            .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+            .Where(x =>
+                x.PropertyType.IsGenericType &&
+                x.PropertyType.GetGenericTypeDefinition() == typeof(DbSet<>));
+        
+        foreach (var dbSet in dbSets)
+            services.AddScoped(dbSet.PropertyType, provider => dbSet.GetValue(provider.GetService<GameDbContext>()));
+    }
+
+    public void OnStartup(object sender, StartupEventArgs e)
+    {
+        var switcher = new WindowSwitcher(_services);
+        switcher.Open<MainWindow>();
     }
 }
